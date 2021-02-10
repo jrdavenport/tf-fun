0556dc7a583c809d5abe605b7ea1fe177336f842
JamesPersonal <jamespersonal@OVO6170MB-MacBook-Pro.local>
2021-02-10 13:59:07 +0000

    Add Terraform planning via circleCI

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
-// Configure the Google Cloud provider
+// Run `gcloud auth application-default login` once to provide credentails
 provider "google" {
- credentials = file("gcp-svc-acc-creds-non-prod.json")
  project     = "distributed-system-non-prod"
  region      = "europe-west2"   
 }
 
-
-// Terraform plugin for creating random ids
 resource "random_id" "instance_id" {
  byte_length = 8
 }
@@ -17,30 +14,6 @@ resource "random_string" "db_id" {
   upper   = false
   number  = true
   special = false
-}
-
-// A single Compute Engine instance
-resource "google_compute_instance" "default" {
- name         = "tf-vm-${random_id.instance_id.hex}"
- machine_type = "f1-micro"
- zone         = "europe-west2-a"
-
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
-
- // Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-
- network_interface {
-   network = "default"
-
-   access_config {
-     // Include this section to give the VM an external ip address
-   }
- }
 }
 
 resource "google_sql_database_instance" "postgresql" {
