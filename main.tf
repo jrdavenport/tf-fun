13795138a4026670aa2bcef949f20f0d16ee8759
JamesPersonal <jamespersonal@OVO6170MB-MacBook-Pro.local>
2021-02-21 15:21:58 +0000

    add bucket

--- main.tf
+++ main.tf
@@ -39,3 +39,22 @@ resource "google_sql_user" "postgresql_user" {
   instance = google_sql_database_instance.postgresql.name
   password = "password"
 }
+
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
