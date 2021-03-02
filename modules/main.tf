414cc00b0de8e8803f8a7ce1207a17b7899e28d4
jamesdavenport <james.davenport@jamesdavenports-MacBook-Pro.local>
2021-03-02 11:55:47 +0000

    Fix AMI version

--- modules/main.tf
+++ modules/main.tf
@@ -34,6 +34,6 @@ resource "google_sql_user" "postgresql_user" {
 
 
 resource "aws_instance" "example" {
-  ami           = "ami-12345678"
+  ami           = "ami-0ffd774e02309201f"
   instance_type = "t2.micro"
 }
